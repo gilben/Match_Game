af8cbd89b2d0fe8cf1c84fc89ca22e337c64ed27
AzureAD\GILBERTOANTONIOCASTR <gilbercastro@live.com>
2018-01-17 19:48:17 -0500

    commit 4

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-///////////////EMPEZAR JUEGO/////////////////////////
 
 $(function() {iniciarJuego();});
 
 
 
-// Comienza el juego
 function iniciarJuego() {
     
     colorRed();
@@ -56,7 +54,6 @@ function iniciarJuego() {
     
 }
 
-//////////// Se llama cuando el juego comienza//////////////////////////////
 
 function tablero() {
 	mostrarDulces();
@@ -66,25 +63,30 @@ function mostrarDulces() {
 	var top = 6;
 	var column = $('[class^="col-"]');
 
-// Obtiene un tipo de caramelo al azar
+
 	column.each(function () {
 		var dulces = $(this).children().length;
 		var agrega = top - dulces;
 		for (var i = 0; i < agrega; i++) {
 			var tipoDulce = getRandomInt(1, 5);
-			// Si la columna está vacía
+		
 			if (i === 0 && dulces < 1) {
 				$(this).append('<img src="image/' + tipoDulce + '.png" class="element"></img>');
 			} else {
 				$(this).find('img:eq(0)').before('<img src="image/' + tipoDulce + '.png" class="element"></img>');
 			}
+            
+       
+            
+           
 		}
 	});
+    $('img').css('height', '7em')
 	addDulcesEvents();
 	validar();
 }
 
-///////////////TERMINAR JUEGO/////////////////////////
+
 
 function finJuego() {
 	$('div.panel-tablero, div.time').effect('fold',1000);
@@ -93,19 +95,14 @@ function finJuego() {
 
 }
 
-///////////////cambia el color del titutlo/////////////////////////
 
-
-
-
-/////////////// Obtiene números aleatorios////////////////
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-////////////////////
+
 function giveCandyArrays(arrayType, index) {
 
 	var Col1 = $('.col-1').children();
@@ -136,37 +133,37 @@ function giveCandyArrays(arrayType, index) {
 	}
 }
 
-//////////Crea filas////////
+
 function candyRows(index) {
 	var candyRow = giveCandyArrays('rows', index);
 	return candyRow;
 }
 
-// Crea columnas
+
 function candyColumns(index) {
 	var candyColumn = giveCandyArrays('columns');
 	return candyColumn[index];
 }
 
-// Valida si hay dulces que se eliminarán en una columna
+
 function columnValidation() {
 	
 	for (var j = 0; j < 7; j++) {
 		var counter = 0;
 		var candyPosition = [];
-		// Salvará la posición de dulces extra
+
 		var extraCandyPosition = [];
-		// Crea una candyColumn
+
 		var candyColumn = candyColumns(j);
-		// Toma el primer objeto de CandyColumn
+
 		var comparisonValue = candyColumn.eq(0);
-		// Se establecerá a true si hay una brecha entre las "líneas de dulces"
+	
 		var gap = false;
-		// Itera sobre el objeto candyColumn
+	
 		for (var i = 1; i < candyColumn.length; i++) {
-			// El src attr de comparisonValue
+		
 			var srcComparison = comparisonValue.attr('src');
-			// El src attr del objeto después de comparisonValue
+		
 			var srcCandy = candyColumn.eq(i).attr('src');
 
 			if (srcComparison != srcCandy) {
@@ -191,20 +188,20 @@ function columnValidation() {
 				}
 				counter += 1;
 			}
-			// Comparación de actualizaciones Valor
+	
 			comparisonValue = candyColumn.eq(i);
 		}
-		// Si la posición extra de caramelo tiene más de dos elementos, se fusionó con la posición de caramelo
+		
 		if (extraCandyPosition.length > 2) {
 			candyPosition = $.merge(candyPosition, extraCandyPosition);
 		}
-		// Si Candy Position tiene menos de / o dos elementos, se elimina
+		
 		if (candyPosition.length <= 2) {
 			candyPosition = [];
 		}
-		// Candy Count es igual al número de elementos en Candy Position
+		
 		candyCount = candyPosition.length;
-		// Si hubiera una "línea de dulces" de 3 o más
+		
 		if (candyCount >= 3) {
 			deleteColumnCandy(candyPosition, candyColumn);
 			setScore(candyCount);
@@ -212,16 +209,16 @@ function columnValidation() {
 	}
 }
 
-// // Añade la clase "eliminar" a "líneas de dulces" que se encuentran en las columnas de dulces
+
 function deleteColumnCandy(candyPosition, candyColumn) {
 	for (var i = 0; i < candyPosition.length; i++) {
 		candyColumn.eq(candyPosition[i]).addClass('delete');
 	}
 }
 
-// Valida si hay dulces que deben eliminarse en una fila
+
 function rowValidation() {
-	// Itera sobre cada fila de dulces
+
 	for (var j = 0; j < 6; j++) {
 		var counter = 0;
 		var candyPosition = [];
@@ -230,9 +227,9 @@ function rowValidation() {
 		var comparisonValue = candyRow[0];
 		var gap = false;
 		for (var i = 1; i < candyRow.length; i++) {
-			// El src attr de comparación Valor
+
 			var srcComparison = comparisonValue.attr('src');
-			// the src attr of the object after comparisonValue
+
 			var srcCandy = candyRow[i].attr('src');
 
 			if (srcComparison != srcCandy) {
@@ -257,20 +254,20 @@ function rowValidation() {
 				}
 				counter += 1;
 			}
-			// Updates comparisonValue
+			
 			comparisonValue = candyRow[i];
 		}
-		// If extraCandyPosition has more than two elements, it's merged with candyPosition
+		
 		if (extraCandyPosition.length > 2) {
 			candyPosition = $.merge(candyPosition, extraCandyPosition);
 		}
-		// If candyPosition has less than/or two elements, it is deleted
+		
 		if (candyPosition.length <= 2) {
 			candyPosition = [];
 		}
-		// CandyCount is equal to the number of elements in candyPosition
+	
 		candyCount = candyPosition.length;
-		// If there was a 'candy line' of 3 or more
+		
 		if (candyCount >= 3) {
 			deleteHorizontal(candyPosition, candyRow);
 			setScore(candyCount);
@@ -278,14 +275,14 @@ function rowValidation() {
 	}
 }
 
-// Agrega la clase 'eliminar' a 'líneas de dulces' en las filas de dulces
+
 function deleteHorizontal(candyPosition, candyRow) {
 	for (var i = 0; i < candyPosition.length; i++) {
 		candyRow[candyPosition[i]].addClass('delete');
 	}
 }
 
-// Establece la puntuación de acuerdo al número de dulces que tienes
+
 function setScore(count) {
 	var score = Number($('#score-text').text());
 	switch (count) {
@@ -313,17 +310,17 @@ function setScore(count) {
 
 
 
-// Establece las validaciones de columnas y fila
+
 function validar() {
 	columnValidation();
 	rowValidation();
-	// Si hay dulces que borrar
+
 	if ($('img.delete').length !== 0) {
 		deletesCandyAnimation();
 	}
 }
 
-// Añade los eventos de dulces
+
 function addDulcesEvents() {
 	$('img').draggable({
 		containment: '.panel-tablero',
@@ -350,7 +347,7 @@ function enableCandyEvents() {
 
 
 
-// Cambia un caramelo por otro
+
 function moverDulce(event, dulceDrag) {
 
 	var dulceDrag = $(dulceDrag.draggable);
@@ -363,9 +360,9 @@ function moverDulce(event, dulceDrag) {
 
 	setTimeout(function () {
 		tablero();
-		// De esta manera, impedimos movimientos equivocados
+		
 		if ($('img.delete').length === 0) {
-			// Caramelo Arrastrar y caramelo Drop se les da su posicion inicial
+			
 			dulceDrag.attr('src', dragSrc);
 			dulceDrop.attr('src', dropSrc);
 		} else {
@@ -374,21 +371,20 @@ function moverDulce(event, dulceDrag) {
 	}, 500);
 
 }
-//revisa el tabelero despues de eliminar
+
 function checkTablero(result) {
 	if (result) {
 		tablero();
 	}
 }
 
-// Actualiza el valor de los movimientos
+
 function updateMoves() {
 	var actualValue = Number($('#movimientos-text').text());
 	var result = actualValue += 1;
 	$('#movimientos-text').text(result);
 }
 
-// Animación que precede a la eliminación de dulces
 function deletesCandyAnimation() {
 	disableCandyEvents();
 	$('img.delete').effect('pulsate', 700);
@@ -409,7 +405,7 @@ function deletesCandyAnimation() {
 			queue: true
 		});
 }
-// Elimina el caramelo 
+
 function deletesDulces() {
 	return new Promise(function (resolve, reject) {
 		if ($('img.delete').remove()) {
